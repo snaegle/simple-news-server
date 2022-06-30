fb29e71b0cdb8c6e770846f196c6693ccb56c119
snaegle <snaegle@users.noreply.github.com>
2022-06-30 20:42:32 +0200

    Update start.sh
    Replace APP_SECRET

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
+#! /bin/bash
+  
+echo "PUTTING APP_SECRET INTO .env FOR LOCAL USE ONLY"
+echo "PLEASE STORE APP_SECRET PERMANENTLY IN .env FOR PRODUCTION!"
+sed -i s/APP_SECRET.*/APP_SECRET=46b88155092e54854cc6653275165852/ symfony/.env
+
 npm install
 docker-compose up -d
 docker-compose exec symfony composer update
